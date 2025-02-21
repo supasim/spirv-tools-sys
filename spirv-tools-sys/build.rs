469260d8f86fa26fc06203b6a0e626bc8bdb31dd
SupaMaggie70Incorporated <magnus.larsson.mn@gmail.com>
2025-02-20 19:55:46 -0600

    Other initial commit

---
+++ spirv-tools-sys/build.rs
@@ -0,0 +1,23 @@
+use std::{env, path::PathBuf};
+
+fn main() {
+    let dir = env::var("SPIRV_TOOLS_DIR").expect("SPIRV_TOOLS_DIR must be set");
+    println!("cargo:rustc-link-search={}/lib", dir);
+    println!("cargo:rustc-link-lib=SPIRV-Tools-opt");
+    println!("cargo:rustc-link-lib=SPIRV-Tools");
+    let bindings = bindgen::Builder::default()
+        // This is recommended I think?
+        .clang_arg("std=c++14")
+        // The input header we would like to generate
+        // bindings for.
+        .headers([format!("{dir}/include/spirv-tools/libspirv.h")])
+        // Tell cargo to invalidate the built crate whenever any of the
+        // included header files changed.
+        .parse_callbacks(Box::new(bindgen::CargoCallbacks::new()))
+        .generate()
+        .expect("Error generating bindings for spirv-tools");
+    let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
+    bindings
+        .write_to_file(out_path.join("bindings.rs"))
+        .expect("Failed to write bindings to file");
+}
